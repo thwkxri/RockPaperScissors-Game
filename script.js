6e14f7287be19a6373d8279a1e9c004930e4cff0
Dmitriy Kari <153106847+thwkxri@users.noreply.github.com>
2024-08-23 22:11:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelectorAll('.game__btn');
+
+const resultEl = document.getElementById('result');
+
+const userScoreEl = document.getElementById('user-score');
+const computerScoreEl = document.getElementById('computer-score');
+
+let userScore = 0;
+let computerScore = 0;
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const result = playRound(button.id, computerPlay());
+    resultEl.textContent = result;
+  })
+})
+
+function computerPlay() {
+  const moves = ['Камень', 'Ножницы', 'Бумага'];
+  const randomMove = Math.floor(Math.random() * moves.length);
+  return moves[randomMove];
+}
+
+function playRound(userMove, computerMove) {
+  if (userMove === computerMove) {
+    return 'Ничья!';
+  }
+  else if (
+    (userMove === 'Камень' && computerMove === 'Ножницы') ||
+    (userMove === 'Бумага' && computerMove === 'Камень') ||
+    (userMove === 'Ножницы' && computerMove === 'Бумага')
+  ) {
+    userScore++;
+    userScoreEl.textContent = userScore;
+    return `Вы победили! ${userMove} бьет ${computerMove}`;
+  } else {
+    computerScore++;
+    computerScoreEl.textContent = computerScore;
+    return `Вы проиграли. ${computerMove} бьет ${userMove}`;
+  }
+}
